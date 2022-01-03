6d6c5d7c8857c32c44194e32a64a4be698f46a4f
Clément Beaubestre <clement.beaubestre@epitech.eu>
2022-01-03 09:03:53 +0100

    FIX | do not display error if undefined

--- src/commands/error.ts
+++ src/commands/error.ts
@@ -1,14 +1,17 @@
 import { Message } from 'discord.js';
 
 export function LogError(msg: Message, error: any): any {
+	if (!error?.response.status)
+		return error;
 	if (error?.response.status >= 400 && error?.response.status < 500) {
 		msg.channel.send(
 			`***${error?.response.status}**: ${error?.response.statusText}*\nPlease update the token for ${error?.response.request.host}`
 		);
 		throw error;
-	} else
+	} else {
 		msg.channel.send(
 			`***${error?.response.status}**: ${error?.response.statusText}*`
 		);
-	return error;
+		return error;
+	}
 }
